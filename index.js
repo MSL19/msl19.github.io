fbce38d0ceb129d1cea596b3f909be9b77be6ccd
MSL19 <40368820+MSL19@users.noreply.github.com>
2018-08-05 21:09:02 -0600

    Color added
    NEW stuff

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var population;
 var lifespan = 400;
 var count = 0;
 var target;
+var genCount = 0;
 var maxforce = 0.1;
 var rx = 100;
 var ry = 150;
 var rw = 200;
 var rh = 10;
+var finished;
 
 function setup() {
     var canvas = createCanvas(400, 300);
@@ -19,10 +21,13 @@ function setup() {
 function draw() {
     background(0);
     population.run();
-    if(count == lifespan){
+    if(count == lifespan||finished){
         population.evaluate();
         population.selection();
         count = 0;
+        genCount++;
+        finished = false;
+        document.getElementById("countDisp").innerHTML = "Generation: "+genCount;
     }
     fill(255);
     rect(100,150,200,10);
@@ -87,13 +92,21 @@ function Population(){
 //30:18
 function DNA(genes){
     if(genes){
+      
         this.genes = genes;
     }
     else{
     this.genes = [];
-    for(var i = 0; i < lifespan; i++){
+    
+    for(var i = 0; i < lifespan+3; i++){
         this.genes[i] = p5.Vector.random2D();
         this.genes[i].setMag(maxforce);
+           
+        
+    }
+    for(var j = lifespan; j<lifespan+3; j++){
+        this.genes[j] = Math.floor(Math.random()*255);
+        console.log(this.genes[j]);
     }
     }
     this.crossover = function(partner){
@@ -113,9 +126,14 @@ function DNA(genes){
 
     this.mutation = function(){
         for(var i=0; i< this.genes.length; i++){
-            if(random(1) < 0.01){
+            if(random(1) < 0.02){
+                if(i<lifespan){
                 this.genes[i] = p5.Vector.random2D();
                 this.genes[i].setMag(maxforce);
+                }
+                else{
+                    this.genes[i] = Math.floor(Math.random()*255);
+                }
             }
         }
     }
@@ -144,15 +162,15 @@ function Rocket(dna) {
             this.fitness = map(d,0,width, width,0);
             if(this.completed){
                 this.fitness *= 10;
+                finished = true;
             }
             if(this.crashed){
-                this.fitness /= 10;
+                this.fitness /= 5;
             }
 
 
     }
 
-    //21:56 i'm tired
     this.update = function(){
         var d = dist(this.pos.x, this.pos.y, target.x, target.y);
         if(d < 10){
@@ -181,7 +199,7 @@ function Rocket(dna) {
     this.show = function () {
         push();
         noStroke();
-        fill(255,150);
+        fill(this.dna.genes[lifespan],this.dna.genes[lifespan+1],this.dna.genes[lifespan+2]);
         translate(this.pos.x, this.pos.y);
         rotate(this.vel.heading());
         rectMode(CENTER);
